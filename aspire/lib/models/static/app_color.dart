b4120029c3f2eeac89d5b11c95687f8a2e8fb7e4
Justin B. Ramas <142135986+senRyuu286@users.noreply.github.com>
2025-11-23 19:02:34 +0800

    Add custom bottom navigation bar with icons
    Introduces a custom bottom navigation bar using a new CustomIcons font and Riverpod state management. Adds supporting files for colors, icons, navigation state provider, and navigation button widget. Updates DummyHomeScreen to use the new navigation bar. Also updates dependencies and pubspec to include the custom font and animations package.

---
+++ aspire/lib/models/static/app_color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static final Color iconGrey = Color(0xFF848DA3);
+  static final Color iconBlue = Color(0xFF4365DE);
+
+
+
+
+  static final Color textGrey = Color(0xFF848DA3);
+  static final Color textBlue = Color(0xFF4365DE);
+}
