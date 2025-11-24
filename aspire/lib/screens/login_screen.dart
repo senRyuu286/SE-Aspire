67b5d061acd8e0a2beab4a67cc8cb8fdfb99f7e3
John Anthony Romeo <jaromeo65@gmail.com>
2025-11-24 20:01:02 +0800

    Added UI design for Login Screen

--- aspire/lib/screens/login_screen.dart
+++ aspire/lib/screens/login_screen.dart
@@ -18,189 +18,229 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('ASPIRE'),),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text('Sign in to your Account', 
-              style: TextStyle(
-              fontSize: 39,
-              fontWeight: FontWeight.w800,
-              ),
-            ),
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        title: Align(
+          alignment: Alignment.centerRight, 
+          child: Text('ASPIRE', style: TextStyle(color: Color(0XFFFFFFFF)),)
+        ), backgroundColor: Color(0XFF172635),),
 
-            Text("Let's sign you in to your account",
-              style: TextStyle(
-                fontSize: 14
-              ),
-            ),
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Image.asset('assets/login_background.png'),
 
-            Form(
-              key: _loginFormKey,
-              child: Column(
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 120,),
+                    Text('Sign in to your Account', 
+                      style: TextStyle(
+                      fontSize: 39,
+                      fontWeight: FontWeight.w800,
+                      color: Color(0XFFFFFFFF)
+                      ),
+                    ),
+
+                    Text("Let's sign you in to your account",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color(0XFFFFFFFF)
+                    ),
+                  ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+              Form(
+                key: _loginFormKey,
+                child: Column(
+                  children: [
+
+                    SizedBox(height: 31,),
+
+                    TextFormField(
+                      controller: _emailPhoneNumberCont,
+                      decoration: InputDecoration(
+                        prefixIcon: Padding(
+                          padding: EdgeInsets.all(0),
+                          child: Icon(
+                            Icons.person_outlined,
+                            color: const Color.fromARGB(151, 158, 158, 158),
+                          ),
+                        ),
+                        label: Text('Email/Phone Number'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50)
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 16,),
+
+                    TextFormField(
+                      controller: _passwordCont,
+                      decoration: InputDecoration(
+                        prefixIcon: Padding(
+                          padding: EdgeInsets.all(0),
+                          child: Icon(
+                            Icons.lock_outlined,
+                            color: const Color.fromARGB(151, 158, 158, 158),
+                          ),
+                        ),
+                        label: Text('Email/Phone Number'),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50)
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 16,),
+
+                    GestureDetector(
+                      onTap: () {
+                        //Place Forgot Password here
+                      },
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text('Forgot password?', 
+                          style: TextStyle(
+                            color: const Color(0xFF4365DE),
+                            fontWeight: FontWeight.w700
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 40,),
+
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0XFF4365DE),
+                        ),
+                        onPressed: () {
+
+                        },
+                        child: Text('Sign in',
+                          style: TextStyle(
+                            color: const Color(0xFFFFFFFF),
+                            fontWeight: FontWeight.w700,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 26,),
+
+              Row(
                 children: [
-                  TextFormField(
-                    controller: _emailPhoneNumberCont,
-                    decoration: InputDecoration(
-                      prefixIcon: Padding(
-                        padding: EdgeInsets.all(0),
-                        child: Icon(
-                          Icons.person_outlined,
-                          color: const Color.fromARGB(151, 158, 158, 158),
-                        ),
-                      ),
-                      label: Text('Email/Phone Number'),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50)
-                      ),
+                  Expanded(
+                    child:  Divider(
+                      color: Colors.grey, // Customize the line color
+                      thickness: 1,
                     ),
                   ),
 
-                  TextFormField(
-                    controller: _passwordCont,
-                    decoration: InputDecoration(
-                      prefixIcon: Padding(
-                        padding: EdgeInsets.all(0),
-                        child: Icon(
-                          Icons.lock_outlined,
-                          color: const Color.fromARGB(151, 158, 158, 158),
-                        ),
-                      ),
-                      label: Text('Email/Phone Number'),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50)
-                      ),
+                  Padding(
+                    padding: EdgeInsets.all(25),
+                    child: Text('Or login with',
+                      style: TextStyle(color: Colors.grey,),
                     ),
                   ),
 
-                  GestureDetector(
-                    onTap: () {
-                      //Place Forgot Password here
-                    },
-                    child: Align(
-                      alignment: Alignment.centerRight,
-                      child: Text('Forgot password?', 
-                        style: TextStyle(
-                          color: const Color(0xFF4365DE),
-                          fontWeight: FontWeight.w700
-                        ),
-                      ),
+                  Expanded(
+                    child:  Divider(
+                      color: Colors.grey, // Customize the line color
+                      thickness: 1,
                     ),
                   ),
+                ]
+              ),
 
+              SizedBox(height: 26,),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
                   SizedBox(
-                    width: double.infinity,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Color(0XFF4365DE),
+                    width:  154,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        side: BorderSide(width: 1, color: Colors.grey),
+                        elevation: 0,
+                        backgroundColor: Colors.white10,
                       ),
                       onPressed: () {
 
                       },
-                      child: Text('Sign in',
-                        style: TextStyle(
-                          color: const Color(0xFFFFFFFF),
-                          fontWeight: FontWeight.w700,
-                          fontSize: 16,
-                        ),
-                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.facebook),
+                          Text('Facebook'),
+                        ],
+                      )
                     ),
                   ),
+
+                  SizedBox(width: 17),
+
+                  SizedBox(
+                    width: 154,
+                    child: TextButton(
+                      style: TextButton.styleFrom(
+                        side: BorderSide(width: 1, color: Colors.grey),
+                        elevation: 0,
+                        backgroundColor: Colors.white10
+                      ),
+                      onPressed: () {
+
+                      },
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.phone_android),
+                          Text('Phone No.'),
+                        ],
+                      )
+                    ),
+                  )
                 ],
               ),
-            ),
 
-            Row(
-              children: [
-                Expanded(
-                  child:  Divider(
-                    color: Colors.grey, // Customize the line color
-                    thickness: 1,
-                  ),
-                ),
+              SizedBox(height: 100,),
 
-                Padding(
-                  padding: EdgeInsets.all(25),
-                  child: Text('Or login with',
-                    style: TextStyle(color: Colors.grey,),
-                  ),
-                ),
-
-                Expanded(
-                  child:  Divider(
-                    color: Colors.grey, // Customize the line color
-                    thickness: 1,
-                  ),
-                ),
-              ]
-            ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(
-                  width:  154,
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      side: BorderSide(width: 1, color: Colors.grey),
-                      elevation: 0,
-                      backgroundColor: Colors.white10,
-                    ),
-                    onPressed: () {
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Don't have an account? "),
+                  GestureDetector(
+                    onTap: () {
 
                     },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.facebook),
-                        Text('Facebook'),
-                      ],
-                    )
-                  ),
-                ),
-
-                SizedBox(width: 17),
-
-                SizedBox(
-                  width: 154,
-                  child: TextButton(
-                    style: TextButton.styleFrom(
-                      side: BorderSide(width: 1, color: Colors.grey),
-                      elevation: 0,
-                      backgroundColor: Colors.white10
-                    ),
-                    onPressed: () {
-
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Icon(Icons.phone_android),
-                        Text('Phone No.'),
-                      ],
-                    )
-                  ),
-                )
+                    child: Text('Register', style: TextStyle(color: Color(0XFF4365DE), fontWeight: FontWeight.w700, )),
+                  )
+                ],
+              )
               ],
             ),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Don't have an account? "),
-                GestureDetector(
-                  onTap: () {
-
-                  },
-                  child: Text('Register', style: TextStyle(color: Color(0XFF4365DE), fontWeight: FontWeight.w700, )),
-                )
-              ],
-            )
-
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
