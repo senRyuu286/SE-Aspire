730e89a114b3c0fc8e3e5fb6446e4a644a73da5d
Justin B. Ramas <142135986+senRyuu286@users.noreply.github.com>
2025-11-26 08:42:27 +0800

    Add skill assessment quiz screen and assets
    Introduced the SkillAssessmentQuiz screen with supporting widgets (QuizAppBar, QuizButton, QuizSectionCard) and added related image assets. Updated initial route and named routes to include the new quiz screen, extended AppColor definitions, and registered assets in pubspec.yaml.

--- aspire/lib/models/static/named_routes.dart
+++ aspire/lib/models/static/named_routes.dart
@@ -1,10 +1,13 @@
 import 'package:aspire/screens/dummy_home_screen.dart';
+import 'package:aspire/screens/skill_assessment_quiz.dart';
 import 'package:flutter/material.dart';
 
 class NamedRoutes {
   static final String homeScreen = '/homeScreen';
+  static final String skillAssessmentQuiz = '/skillAssessmentQuiz';
 
   static Map<String, Widget Function(BuildContext)> routes = {
     homeScreen: (context) => const DummyHomeScreen(), 
+    skillAssessmentQuiz: (context) => const SkillAssessmentQuiz(),
   };
 }
